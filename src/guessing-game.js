d068524ce82f54231621570d441e38dd55770da3
AlexandrFugin <fav2.7@mail.ru>
2022-08-14 22:13:23 +0300

    fix: solved the function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        //задает начальное значение 
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        //определяет переменные 
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        //выводит результат 
+        // console.log(Math.round((this.min + this.max) / 2));
+        return Math.round((this.min + this.max) / 2);
+         
     }
 
     lower() {
-
+        //обновляет максимальное число
+        this.max = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
-
+        //обновляет минимальное число
+        this.min = Math.round((this.min + this.max) / 2);
     }
 }
 
